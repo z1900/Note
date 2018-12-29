a52113e2b0a38110298006eb9c64538388305d95
z1900 <z1900.dev@gmail.com>
2018-12-29 10:20:10 +0800

    change directory structure

--- JavaScript/sort/quickSort.js
+++ JavaScript/sort/quickSort.js
@@ -1,27 +1,28 @@
 var arr = [1 ,4, 2, 7, 11, 5, 8]
-        function quickSort(arr) {
-            if (arr.length <= 1) {
-                return arr
-            }
 
-            var mid = Math.ceil(arr.length / 2),
-                midItem = arr.splice(mid, 1)[0],
-                left = [],
-                right = []
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr
+    }
 
-            arr.forEach(function(item) {
-                if (item <= midItem) {
-                    left.push(item)
-                } else {
-                    right.push(item)
-                }
-            })
+    var mid = Math.ceil(arr.length / 2),
+        midItem = arr.splice(mid, 1)[0],
+        left = [],
+        right = []
 
-            var _left = quickSort(left),
-                _right = quickSort(right)
-
-            return _left.concat(midItem, _right)
+    arr.forEach(function(item) {
+        if (item <= midItem) {
+            left.push(item)
+        } else {
+            right.push(item)
         }
+    })
 
-        console.log(quickSort(arr))
+    var _left = quickSort(left),
+        _right = quickSort(right)
+
+    return _left.concat(midItem, _right)
+}
+
+console.log(quickSort(arr))
         
